f4c03a08a8163629234b35955f50a78b37a36e97
Weiyang Wang <wew168@ucsd.edu>
2018-02-19 21:54:34 -0800

    SIMmodule

--- simmodule.py
+++ simmodule.py
@@ -79,3 +79,63 @@ class SIMModule(object):
             self.connected = False
 
 
+class SIM921(SIMModule):
+    """
+    SIM921 class represents an sim921 module connected to the mainframe.
+    """
+
+    def setExcitationOnOff(self, state):
+        self.send('EXON {0}'.format(state))
+
+    def setExcitationRange(self, e_int):
+        self.send('EXCI {0}'.format(e_int))
+
+    def queryExcitation(self):
+        return self.query('EXON?')
+
+    def setResistanceRange(self, r_int):
+        self.send('RANG {0}'.format(r_int))
+
+    def setExcitationFreq(self, freq):
+        self.send('FREQ {0}'.format(freq))
+
+    def getExcitationFreq(self):
+        return self.query('FREQ?')
+
+    def setTimeConstant(self, tau):
+        self.send('TCON {0}'.format(tau))
+
+    def getTimeConstant(self):
+        return self.query('TCON?')
+
+
+class SIM922(SIMModule):
+    """
+    SIM922 class represents a sim922 module connected to the mainframe.
+    """
+    def getDiodeVoltages(self):
+        volts = self.query('VOLT? 0,1')
+        volts = volts.split(',')
+        try:
+            voltages = map(float, volts)
+        except ValueError:
+            voltages = [float('nan')]
+        return voltages
+
+    def getDiodeTemps(self):
+        temps = self.query('TVAL? 0,1')
+        temps = temps.split(',')
+        try:
+            temperatures = map(float, temps)
+        except ValueError:
+            temperatures = [float('nan')]
+        return temperatures
+
+
+class SIM925(SIMModule):
+    """
+    SIM925 class represents an SIM925 module connected to the main frame.
+    """
+
+    def SIM925switchMUX(self,channel):
+        self.send('CHAN {0}'.format(channel))
